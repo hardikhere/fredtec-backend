bceb5c76ac267006ab293f24b4f94b3e952f65e8
hardikhere <web.hardikhere@gmail.com>
2021-01-02 17:34:21 +0530

    installed helmet

--- app.js
+++ app.js
@@ -7,17 +7,30 @@ require("dotenv").config();
 const app = express();
 const SchoolRoutes = require("./Routes/School");
 const SendResponse = require("./utils/Responses");
+const helmet = require("helmet");
 connect_db();// connect to mongoDB
 
+var whitelist = ['http://localhost:3000/', 'http://localhost:8080/'];
+var corsOptions = {
+    origin: function (origin, callback) {
+        if(!origin) return callback(null, true);
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
 
 //app middlewares
-app.use(cors());
+app.use(helmet());
+app.use(cors(corsOptions));
 app.use(
     bodyParser.urlencoded({
         extended: false,
     }));
 app.use(bodyParser.json())
-app.use(morgan("dev"));
+app.use(morgan(process.env.NODE_ENV || "dev"));
 
 //API routes
 const BaseAPI = "/api/v1";
@@ -26,6 +39,7 @@ app.use(BaseAPI, SchoolRoutes);
 app.use("/health-check", (req, res) => {
     SendResponse(res, 200, {}, "Healthy");
 });
+
 
 
 
