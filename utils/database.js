86eac0b7785d5d48e1b8e03ab16e9fd7c18cf5c8
hardik khanchandani <51117080+hardikhere@users.noreply.github.com>
2021-06-03 19:11:42 +0530

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-//"mongodb+srv://hardik:hardikisbest009@cluster0.iqpr4.mongodb.net/fredmat?retryWrites=true&w=majority"
+
 function connect_db(){
     try{
-        mongoose.connect("mongodb+srv://hardik:hardikisbest009@cluster0.iqpr4.mongodb.net/fredmat?retryWrites=true&w=majority",
+        mongoose.connect(process.env.DATABASE,
         { useNewUrlParser: true,
           useUnifiedTopology: true,
           useCreateIndex:true,
